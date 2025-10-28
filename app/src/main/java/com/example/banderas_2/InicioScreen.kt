4a90f6e13a0be2883fffcf91af86d9f28daa20e4
sara saldivar <sarasaldivar472@gmail.com>
2025-10-27 23:06:07 -0600

    verdesote

--- app/src/main/java/com/example/banderas_2/InicioScreen.kt
+++ app/src/main/java/com/example/banderas_2/InicioScreen.kt
@@ -1,2 +1,20 @@
 package com.example.banderas_2
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+
+@Composable
+fun MexicoBandera(modifier: Modifier = Modifier){
+    Row(modifier= modifier){
+        Box (modifier=Modifier
+            .weight(1f)
+            .fillMaxHeight()
+            .background(Color(0xFF006847))
+        )
+    }
+}
