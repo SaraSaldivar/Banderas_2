38045ee2fc8468fd844ae8f4bcde610fd88f2d14
sara saldivar <sarasaldivar472@gmail.com>
2025-10-27 23:56:52 -0600

    se me olvidó q teníamos escudo xdddd apartre le agregué un preview pq digamos q tardó mucho en mi cel ._.

--- app/src/main/java/com/example/banderas_2/InicioScreen.kt
+++ app/src/main/java/com/example/banderas_2/InicioScreen.kt
@@ -1,5 +1,6 @@
 package com.example.banderas_2
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
@@ -10,34 +11,45 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun MexicoBandera(modifier: Modifier = Modifier){
-    Row(modifier= modifier){
-        Box (modifier=Modifier
-            .weight(1f)
-            .fillMaxHeight()
-            .background(Color(0xFF006847))
-        )
-        Box(modifier=Modifier
-            .weight(1f)
-            .fillMaxHeight()
-            .background(Color.White)
-        )
-        Box(modifier= Modifier
-            .weight(1f)
-            .fillMaxHeight()
-            .background(Color(0xFFCE1126))
+    Box(modifier= modifier){
+        Row(modifier= modifier){
+            Box (modifier=Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFF006847))
+            )
+            Box(modifier=Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color.White)
+            )
+            Box(modifier= Modifier
+                .weight(1f)
+                .fillMaxHeight()
+                .background(Color(0xFFCE1126))
+            )
+        }
+        Image(
+            painter = painterResource(id = R.drawable.escudomexico),
+            contentDescription = "escudo",
+            modifier=Modifier.align(Alignment.Center).size(80.dp)
+
         )
     }
 }
 
+@Preview
 @Composable
 fun MostrarBanderaMX(){
     Box(modifier = Modifier
         .fillMaxSize()
-        .background(Color.LightGray),
+        .background(Color.White),
         contentAlignment = Alignment.Center
     ){
         MexicoBandera(
